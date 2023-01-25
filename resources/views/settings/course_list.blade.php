90d3c8ac7cf9a9806d1692dc7d1ea796c7608f15
Karthika V <Karthika.V@stellaripl.com>
2023-01-25 16:57:49 +0530

    Code Updated

--- resources/views/settings/course_list.blade.php
+++ resources/views/settings/course_list.blade.php
@@ -285,6 +285,19 @@ $(document).ready(function()
         }
   	});
 
+    CKEDITOR.on("instanceReady", function(event) {
+        event.editor.on("beforeCommandExec", function(event) {
+            // Show the paste dialog for the paste buttons and right-click paste
+            if (event.data.name == "paste") {
+                event.editor._.forcePasteDialog = true;
+            }
+            // Don't show the paste dialog for Ctrl+Shift+V
+            if (event.data.name == "pastetext" && event.data.commandData.from == "keystrokeHandler") {
+                event.cancel();
+            }
+        })
+    });
+
     var table = $('#datatable').DataTable({
         responsive: true,
         dom: 'l<"toolbar">frtip',
